b7d047d45afdfefdc891f9959903914bb050881e
Andreas Wälchli <andreas.waelchli@me.com>
2018-05-16 12:01:33 +0200

    added websocked core

--- src/main/java/ch/awae/gameserver/Application.java
+++ src/main/java/ch/awae/gameserver/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ch.awae.gameserver.core.User;
 import ch.awae.gameserver.core.UserRepo;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
